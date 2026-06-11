7f801352b0681e17292a1b2177839a729063b40e
Daniel Sousa <danielcabritasousa@hotmail.com>
2026-06-12 00:19:59 +0100

    Updated test suite

--- code/services/marketprice/repository/marketprice_repository_test.go
+++ code/services/marketprice/repository/marketprice_repository_test.go
@@ -16,9 +16,9 @@ func TestMarketPriceRepository_GetAverageMarketPrice_WithFilters(t *testing.T) {
 
 	repo := NewRepository(db)
 
-	query := "SELECT\\s+COALESCE\\(AVG\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MIN\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MAX\\(ad.ask_price\\), 0\\),\\s+COUNT\\(ad.ask_price\\).*WHERE 1=1 AND b.name = \\$1 AND m.name = \\$2 AND ad.model_year >= \\$3 AND ad.model_year <= \\$4"
+	query := "SELECT\\s+COALESCE\\(AVG\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MIN\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MAX\\(ad.ask_price\\), 0\\),\\s+COUNT\\(ad.ask_price\\).*WHERE 1=1 AND LOWER\\(b.name\\) = \\$1 AND LOWER\\(m.name\\) = \\$2 AND ad.model_year >= \\$3 AND ad.model_year <= \\$4"
 	mock.ExpectQuery(query).
-		WithArgs("FORD", "Fiesta", int32(2010), int32(2020)).
+		WithArgs("ford", "fiesta", int32(2010), int32(2020)).
 		WillReturnRows(sqlmock.NewRows([]string{"avg", "min", "max", "count"}).
 			AddRow(12000.0, 9000.0, 15000.0, int32(3)))
 
--- code/services/marketprice/service/marketprice_service_test.go
+++ code/services/marketprice/service/marketprice_service_test.go
@@ -16,9 +16,9 @@ func TestMarketPriceService_GetAverageMarketPrice(t *testing.T) {
 	}
 	defer db.Close()
 
-	query := "SELECT\\s+COALESCE\\(AVG\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MIN\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MAX\\(ad.ask_price\\), 0\\),\\s+COUNT\\(ad.ask_price\\).*WHERE 1=1 AND b.name = \\$1 AND m.name = \\$2"
+	query := "SELECT\\s+COALESCE\\(AVG\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MIN\\(ad.ask_price\\), 0\\),\\s+COALESCE\\(MAX\\(ad.ask_price\\), 0\\),\\s+COUNT\\(ad.ask_price\\).*WHERE 1=1 AND LOWER\\(b.name\\) = \\$1 AND LOWER\\(m.name\\) = \\$2"
 	mock.ExpectQuery(query).
-		WithArgs("FORD", "Fiesta").
+		WithArgs("ford", "fiesta").
 		WillReturnRows(sqlmock.NewRows([]string{"avg", "min", "max", "count"}).
 			AddRow(12000.0, 9000.0, 15000.0, int32(3)))
 
